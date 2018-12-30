b66b403d83206319f94430162375379b3994415b
zep1994 <snaxbags@protonmail.com>
2018-12-30 10:43:18 -0600

    Add folder setup and layout component

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -79,8 +79,6 @@ module.exports = function(webpackEnv) {
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
-        modules: true,
-        localIdentName: '[name]__[local]__[hash:base64:5]'
       },
       {
         // Options for PostCSS as we reference these options twice
