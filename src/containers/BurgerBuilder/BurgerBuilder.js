4c7357d48e89b4fd78840878b76cafe667fc086c
zep1994 <snaxbags@protonmail.com>
2018-12-30 17:38:25 -0600

    Add Remove Feature

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,10 +39,31 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
-
+        const oldCount = this.state.ingredients[type]
+        if (oldCount <= 0) {
+            return
+        }
+        const updatedCount = oldCount - 1
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount
+        const priceDeduction = INGREDIENT_PRICES[type]
+        const oldPrice = this.state.totalPrice
+        const newPrice = oldPrice - priceDeduction
+        this.setState({
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        } 
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
                 <Burger 
@@ -50,6 +71,8 @@ class BurgerBuilder extends Component {
                 />
                 <BuildControls 
                 ingredientAdded={this.addIngredientHandler}
+                ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}
                 />
             </Aux>
         )
