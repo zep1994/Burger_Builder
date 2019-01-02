3899bdd55f6402943462874424564afc1890ed66
zep1994 <snaxbags@protonmail.com>
2019-01-02 16:49:09 -0600

    Update File structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import Aux from '../../../HOC/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../HOC/AUX/Aux';
 import Button from '../../UI/Button/Button'
 
-const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-        return <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}:</span> {props.ingredients[igKey]}
-               </li>
-    })
-    return(
+class OrderSummary extends Component {
+    //could be functional
+    componentWillUpdate() {
+        console.log('[OrderSummary]Will update')
+    }
+
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}:</span> {this.props.ingredients[igKey]}
+                   </li>
+        })
+        return (
         <Aux>
             <p>Delicious Burger With The Following Ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+            <p><strong>Total Price: {this.props.price}</strong></p>
             <p>Continue To Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+             <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+             <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
+        )
+    }
 }
 
 export default OrderSummary
