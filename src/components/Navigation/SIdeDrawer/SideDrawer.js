9f69200030028eff2651766d5601c367fd3cdd7d
zep1994 <snaxbags@protonmail.com>
2019-01-02 10:47:50 -0600

    Add media query for mobile devices

--- src/components/Navigation/SIdeDrawer/SideDrawer.js
+++ src/components/Navigation/SIdeDrawer/SideDrawer.js
@@ -1,19 +1,29 @@
 import React from 'react';
+
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.css'
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../HOC/Aux';
 
+const sideDrawer = ( props ) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+};
 
-const sideDrawer = (props) => {
-    return(
-        <div className={classes.SideDrawer}>
-            <Logo />
-            <nav>
-                <NavigationItems />
-            </nav>
-           
-        </div>
-    )
-}
-
-export default sideDrawer
+export default sideDrawer;
